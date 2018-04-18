9a5606b0c4feeb9b0094a44461ba54e8b94eb9f7
MEI CHEN <MEICHEN6@GMAIL.COM>
2018-04-18 10:20:23 -0400

    got part of the calender to appear

--- frontend/src/pages/Calendar.js
+++ frontend/src/pages/Calendar.js
@@ -1,5 +1,16 @@
 import React from 'react'
+import BigCalendar from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import moment from 'moment'
 
-const Calendar = () => <div>Calendar</div>
+// Setup the localizer by providing the moment (or globalize) Object
+// to the correct localizer.
+BigCalendar.momentLocalizer(moment) // or globalizeLocalizer
+
+const Calendar = () => (
+	<div>
+		<BigCalendar events={[]} startAccessor="startDate" endAccessor="endDate" />
+	</div>
+)
 
 export default Calendar
